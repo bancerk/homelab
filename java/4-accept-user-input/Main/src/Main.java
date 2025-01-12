8098721c246cbd397672c774fafb78843a7bacca
bancerk <192424850+bancerk@users.noreply.github.com>
2025-01-12 13:20:08 +0300

    user input

---
+++ java/4-accept-user-input/Main/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    Scanner scanner = new Scanner(System.in);        
+
+    System.out.println("What is your name ? ");
+    
+    String name = scanner.nextLine();
+
+    System.out.println("Hello " +name +".");
+
+    System.out.println("How old are you ? ");
+
+    int age = scanner.nextInt();
+
+    System.out.println("You are " + age +" years old.");
+    
+    scanner.nextLine();
+
+    System.out.println("What is your favorite food ? ");
+
+    String food = scanner.nextLine();
+
+    System.out.println("Your favorite food is " + food + ".");
+
+    }
+}
