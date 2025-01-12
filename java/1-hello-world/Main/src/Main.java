5bd914954b184b17fa17f9f18f74d68b1237208d
bancerk <192424850+bancerk@users.noreply.github.com>
2025-01-12 12:43:35 +0300

    init Java

---
+++ java/1-hello-world/Main/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // This is a comment
+        // start coding here
+        
+        System.out.print("Hello, World!");
+        System.out.print("I love pizza");
+
+        System.out.print("Hello, World!\t");
+        System.out.print("\nI love pizza");
+
+        // \n - new line
+        // \t - tab
+
+        System.out.println("Hello, World!");
+        System.out.println("I love pizza");;
+
+        // println - print to new line
+        // print - print to same line
+
+        /* this is a series of comments
+         * that span multiple lines
+         * and are ignored by the compiler
+         */
+
+         // just type 'sysout' and press tab for IDE to auto complete to System.out.println();
+    }
+}
---
+++ java/2-variables/Main/src/Main.java
@@ -0,0 +1,17 @@
+package Main.src;
+public class Main {
+
+    public static void main(String[] args) {
+        // declaration + assignment
+
+        int x;
+        x = 123;
+
+        // or combine
+
+        int y = 5;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
